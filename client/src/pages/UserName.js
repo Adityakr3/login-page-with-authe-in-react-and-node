0ef3822e118b38c608821d2875968de5b7a8f116
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-21 23:00:22 +0530

    fully function working with get data with useformik

--- client/src/pages/UserName.js
+++ client/src/pages/UserName.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import {Toaster} from "react-hot-toast"
 import image from "../asset/images/pic.webp"
 import style from "../style/UserName.module.css"
-
+import {useFormik} from 'formik'
+import { usernameValidate } from "../helper/Validate";
 export function UserName() {
-  const [email , setEmail] = useState("");
-  const SendOtp = (e) => {
-    e.preventDefault();
-    if(email === ""){
-        toast.error("ENTER A VALID MAIL")
-    }
-    else if(!email.includes('@')){
-        toast.error("PLEASE ADD A VALID MAIL")
-    }
-    else{
-        toast.success("Login Done 😄")
-    }
-  };
+
+const formik = useFormik({
+   initialValues:{
+    username:'',
+   },
+   validate:usernameValidate,
+   validateOnBlur:false,
+   validateOnChange:false,
+   onSubmit: async values => {
+     console.log(values);
+   }
+})
 
   return (
     <div className={style.main}>
+      <Toaster className='top-center' reverseOrder={false}></Toaster>
       <div id={style.container}>
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -33,32 +34,30 @@ export function UserName() {
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form onSubmit={formik.handleSubmit} className="space-y-6" action="#" method="POST">
             <div className={style.avtar}>
                 <img src={image} alt="" srcset="" />
             </div>
             <div>
               <div className="mt-2">
-                <input
+                <input {...formik.getFieldProps('username') }
                   placeholder="Enter Your UserName"
-                  onChange={(e) => setEmail(e.target.value)}
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
+                  className="block w-full  p-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
             <div>
-              <button
-                onClick={SendOtp}
+              <Link to='/password'
+                // onClick={SendOtp}
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Submit
-              </button>
+              </Link>
             </div>
           </form>
 
@@ -73,7 +72,6 @@ export function UserName() {
           </p>
         </div>
       </div>
-      <ToastContainer />
     </div>
   );
 }

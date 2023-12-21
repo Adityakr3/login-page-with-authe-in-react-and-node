0ff9761294914b0c249c2af6357e044302b81135
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-22 00:03:47 +0530

    image upload function

---
+++ client/src/helper/Convert.js
@@ -0,0 +1,14 @@
+// image convert to base64
+
+export default function converttobase64(file){
+    return new Promise((resolve , reject)=> {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            resolve(fileReader.result)
+        }
+        fileReader.onerror = (error) =>{
+            reject(error)
+        }
+    })
+}

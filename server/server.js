0c3639cf1f1d6aef85f9d4ae502bfb1a9af486ab
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-24 23:24:13 +0530

    connect routes with middleware to controller

--- server/server.js
+++ server/server.js
@@ -1,22 +1,38 @@
-require("dotenv").config();
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connect from "./database/connection.js";
+import router from './router/router.js'
 const app = express();
-const cors = require("cors");
-const morgan = require("morgan")
-
 
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
-app.disable('x-powered-by')
+app.disable('x-powered-by');
 
 const PORT = 3030;
 
+
+
+//hhtp get reqest //
 app.get("/",(req , res)=>{
-   res.status(200).json("server is start")
+   res.status(201).json("server is start")
 })
+
+// api router 
+
+app.use('/api',router)
+
 //******************* start server ******************** //
-app.listen( PORT , ()=>{
-    console.log(`SERVER IS RUNNING AT http://localhost:${PORT}`);
+connect().then(()=> {
+    try {
+        app.listen( PORT , ()=>{
+            console.log(`SERVER IS RUNNING AT http://localhost:${PORT}`);
+        }) 
+    } catch (error) {
+        console.log('can not connect with server ');
+    }
+}).catch(error => {
+    console.log(error);
 })

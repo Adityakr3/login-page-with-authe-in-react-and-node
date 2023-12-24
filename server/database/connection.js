ac6700db60844a03f04f0df14ba35bcbebfe8130
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-24 23:22:32 +0530

    db connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,2 +1,13 @@
-const mongoose = require("mongoose")
-const MongoMemoryServer = require("mongodb-memory-server")
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+async function connect() {
+  const mongodb = await MongoMemoryServer.create();
+//   const password = encodeURIComponent("adityakumar")
+//   const getUri = mongodb.getUri(`mongodb+srv://adi12:${password}@adi-database.xth9owk.mongodb.net/?retryWrites=true&w=majority`);
+  const getUri = mongodb.getUri("mongodb://localhost:27017")
+  mongoose.set("strictQuery", true);
+  const db = await mongoose.connect(getUri);
+  console.log("Database Connected");
+  return db;
+}
+export default connect;
